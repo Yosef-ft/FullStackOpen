53ee3d1e91b374121756bc2c3aac3abb640a6a61
Yosef-ft <yosefetene02@gmail.com>
2024-12-10 10:29:03 +0300

    modified main logic of the app

--- part2/countriesData/src/App.jsx
+++ part2/countriesData/src/App.jsx
@@ -1,96 +1,45 @@
 import { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
+
+import Filter from './components/Filter'
+import Content from "./components/Content";
 
 const App = () =>{
-  const [allContries, setAllCountries] = useState(null)
-  const [search, setSearch] = useState('')
-  const [detail, setDetail] = useState(null);
+    const [allCountries, setAllCountries] = useState(null)
+    const [searchValue, setSearchValue] = useState('')
+    const [countries, setCountries] = useState([])
 
-  useEffect(()=>{
-    axios
-      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
-      .then(response =>{
-        setAllCountries(response.data)
-        
+    useEffect(() =>{
+        axios
+        .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+        .then(response =>{
+          setAllCountries(response.data)
+        })
+        .catch(error => {
+            console.log('Error', error)
+        })
+    }, [])
 
 
-      })
-  }, [])
+    const handleChange = (event) => {
+        const value = event.target.value
+        setSearchValue(value)
 
-
-  const showView  = (country) =>{
-    const oneCountry = allContries.filter(countryData => countryData.name.common == country)
-    const languages = Object.values(oneCountry[0].languages) 
-    console.log(oneCountry)
-    const detailContent = <div>
-                <h2><strong>{country}</strong></h2> 
-                <p>capital {oneCountry[0].capital}</p>
-                <p>area {oneCountry[0].area}</p>
-                <p><strong>languages:</strong></p>
-                <ul>
-                {languages.map(lang => <li key={lang}>{lang}</li>)}
-                </ul>
-                <img src={oneCountry[0].flags.png} />
-              </div>
-  setDetail(detailContent)              
-  }
-
-  let message = ''
-
-  if (allContries){
-    const countriesName = allContries.map(country => country.name.common)
-    const filtered = countriesName.filter(name => name.toLowerCase().includes(search.toLowerCase()))
-    console.log(filtered)
-
-    
-    if (filtered.length > 10) {
-      message = 'Too many matches, spacifiy another filter'
+        if (value){
+            const filteredCountries = allCountries.filter(country => country.name.common.toLowerCase().includes(value.toLowerCase()))
+            setCountries(filteredCountries)
+        }else{
+            setCountries([])
+        }
     }
-    else if (filtered.length === 1){
-      console.log('filteren length', filtered.length)
-      const oneCountry = allContries.filter(country => country.name.common == filtered[0])
-      const languages = Object.values(oneCountry[0].languages) 
-      console.log(oneCountry)
-      message = <div>
-                  <h2><strong>{filtered[0]}</strong></h2> 
-                  <p>capital {oneCountry[0].capital}</p>
-                  <p>area {oneCountry[0].area}</p>
-                  <p><strong>languages:</strong></p>
-                  <ul>
-                  {languages.map(lang => <li key={lang}>{lang}</li>)}
-                  </ul>
-                  <img src={oneCountry[0].flags.png} />
-                </div>
-    }
-    else{
-      message = filtered.map(country => <div>
-                                          {country} <button onClick={() =>showView(country)}>show</button>
-                                          
-                                        </div>
-                            );
-    }
-  }
 
-  const handleChange = (event) =>{
+    return(
+        <div>
+            <Filter searchValue={searchValue} handleChange={handleChange} />
+            <Content countries={countries} setCountries={setCountries} />            
+        </div>
 
-    setSearch(event.target.value)
-    setDetail(null)
-  }
-
-
-  if (search === '') {
-    message = ''
-  }
-
-  return(
-    <div>
-      <label>Find Countires</label>
-      <input onChange={handleChange} /><br />
-      {message}
-      {detail}
-    </div>
-  )
-
+    )
 }
 
 
